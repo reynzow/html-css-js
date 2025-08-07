8e25475ac0220a979b6f9fa0cd46d38a3b608756
Renzo Cabral <rnzcbrl@gmail.com>
2025-08-07 14:00:13 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    alert("Thank you for your message!");
+}
+
+function highlightPortfolio() {
+    document.getElementById("portfolio").style.backgroundColor = "#f0f8ff";
+}
